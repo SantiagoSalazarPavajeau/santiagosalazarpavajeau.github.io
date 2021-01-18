6b4f6e2aa5f4806e7ee298c798d5e227a78e1fb5
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2021-01-18 15:59:23 +0100

    add initial setup to portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="header">
+          <header className="header">
+
+          </header>
+          <div className="profile-card">
+              <div className="pancake">
+                <div className="profile-image"/>
+                <div className="bio">
+                  <p>Hello, I am </p> <h1>Santiago Salazar Pavajeau</h1> <p>I am a software engineer with a background in business and science with a passion for collaboration and innovative technical projects.</p>
+                  <button><a href="https://www.linkedin.com/in/santiago-salazar-pavajeau/" >LinkedIn</a></button> 
+                  <button><a href="https://twitter.com/santispavajeau">Twitter</a></button>
+                  <button>Resume</button>
+                  <button>Blog</button>
+                </div>
+              </div>
+          </div>
+          <header className="header2">
+
+          </header>
+
+        </div>
       </header>
     </div>
   );
